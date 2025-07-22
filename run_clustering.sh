6a0fdfcbcc8b35497fa8e261eea4142267e4939f
chlohamp <champ007@fiu.edu>
2025-07-22 17:03:18 -0400

    testing updates to clustering scripts

--- run_clustering.sh
+++ run_clustering.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
-#SBATCH --job-name=clustering
+#SBATCH --job-name=kmeans
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem-per-cpu=4gb
 #SBATCH --account=iacc_nbc
 #SBATCH --qos=pq_nbc
 #SBATCH --partition=IB_40C_512G
 # Outputs ----------------------------------
-#SBATCH --output=log/%x_%j.out
-#SBATCH --error=log/%x_%j.err
+#SBATCH --output=/home/data/nbc/misc-projects/meta-analyses/abide-hb-meta/log/%x/%x_%A-%a.out
+#SBATCH --error=/home/data/nbc/misc-projects/meta-analyses/abide-hb-meta/log/%x/%x_%A-%a.err
 # ------------------------------------------
+# Max # CPUs = 360
+# modified from ABIDE workflow
 
 # IB_44C_512G, IB_40C_512G, IB_16C_96G, for running workflow
 # investor, for testing
 pwd; hostname; date
+set -e
 
 #==============Shell script==============#
 # Load evironment
-source /home/data/nbc/misc-projects/meta-analyses/Hampson_rdoc-meta-analysis/env/activate_env
-set -e
+module load miniconda3-4.5.11-gcc-8.2.0-oqs2mbg
+source activate /home/champ007/kmeans_env
 
-PROJECT_DIR="/home/data/nbc/misc-projects/meta-analyses/Hampson_rdoc-meta-analysis"
+PROJECT_DIR="/home/data/nbc/misc-projects/meta-analyses/abide-hb-meta"
 
 # Setup done, run the command
 cmd="python ${PROJECT_DIR}/kmeans.py \
